4ef62c9c9baf27dabac08babcfae019f4d6ea14a
ChristopherE2 <cballestinf@gmail.com>
2022-11-14 00:57:24 +0100

    11/11/2022

--- scripts/crossepg-arm.sh
+++ scripts/crossepg-arm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Provides: ChristopherE2
-# Description: Script para actualización de epg
-# Version: 1.0
-# Date: 22/10/2022
+# Description: Script para actualización de epg arm
+# Version: 1.1
+# Date: 12/11/2022
 
 ping -c 1 github.com
 if [ $? -ne 1 ];
@@ -13,12 +13,15 @@ then
 	wget https://github.com/ChristopherE2/crossepg/raw/main/arm.zip
 	unzip arm.zip
 	##Providers Install##
+	rm -rf /usr/crossepg/providers/movistarepgprovider.conf
 	mv /tmp/arm/providers/movistarepgprovider.conf /usr/crossepg/providers
 	##Scripts Install##
+	rm -rf /usr/crossepg/scripts/movistarepgdownload
 	mv /tmp/arm/scripts/movistarepgdownload /usr/crossepg/scripts
 	chmod 777 /usr/crossepg/scripts/movistarepgdownload/movistarepgdownload
 	chmod 777 /usr/crossepg/scripts/movistarepgdownload/movistarepgdownload.sh
 	##Crossepg.config Install##
+	rm -rf /usr/crossepg/crossepg.config
 	mv /tmp/arm/crossepg.config /usr/crossepg
 	echo "¡epg actualizado con éxito!"
 else
